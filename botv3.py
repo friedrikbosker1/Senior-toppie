e0c38172be2ae35923ace8ab2f49f1c864cbec54
friedrikbosker1 <43855536+friedrikbosker1@users.noreply.github.com>
2021-02-24 12:55:38 +0100

    Update botv3.py

--- botv3.py
+++ botv3.py
@@ -24,19 +24,19 @@ async def on_message(message):
     elif message.content.startswith('!meesterwerk'):
         await client.send_message(message.channel, 'https://soundcloud.com/wissai/je-moeder-vind-jou-een-loeder/s-TU4Nz')
     elif message.content.startswith('cibrin'):
-        await client.send_message(message.channel, '\U0001F308 homo \U0001F308')
+        await client.send_message(message.channel, 
     elif message.content.startswith('job'):
-        await client.send_message(message.channel, '(==lullo==3')
+        await client.send_message(message.channel, 
     elif message.content.startswith('wisse'):
-        await client.send_message(message.channel, 'Hipster SJW grote kin nerd haha loeder')
+        await client.send_message(message.channel, 
     elif message.content.startswith('friedrik'):
         await client.send_message(message.channel, 'Bedoelde u: Friederik, Frederik of Friedrich?')
     elif message.content.startswith('arne'):
         await client.send_message(message.channel, 'https://www.youtube.com/user/ArneTechTips powered by RutgerVlogs \n http://i.imgur.com/cwYpevi.png')
     elif message.content.startswith('levi'):
-        await client.send_message(message.channel, 'Gore bloedhond')
+        await client.send_message(message.channel,
     elif message.content.startswith('liam'):
-        await client.send_message(message.channel, 'Alle gierige joden moeten terug naar Israel')
+        await client.send_message(message.channel, 
     elif message.content.startswith('jehannes'):
         await client.send_message(message.channel, "Stemmetje in Job's hoofd")
     elif message.content.startswith('jordy'):
@@ -48,7 +48,7 @@ async def on_message(message):
     elif message.content.startswith('konjo'):
         await client.send_message(message.channel, 'Wollah g', tts=True)
     elif message.content.startswith('pimmel'):
-        await client.send_message(message.channel, 'Pimmel pammel pommel pam, kijk eens wat mijn pimmel kan', tts=True)
+        await client.send_message(message.channel, 
     elif message.content.startswith(''):
         await client.add_reaction(message,discord.utils.get(client.get_all_emojis(), name = 'seniortoppie'))
 
